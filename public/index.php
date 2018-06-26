e8a2a30ce77d40fcfc1d50f693755626e5f66364
MilanAjtic <milanajtic@gmail.com>
2018-06-26 19:21:57 +0200

    salje response

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ use FastRoute\RouteCollector;
 use Middlewares\FastRoute;
 use Middlewares\RequestHandler;
 use Relay\Relay;
+use Zend\Diactoros\Response;
 use Zend\Diactoros\ServerRequestFactory;
 use function DI\create;
 use function DI\get;
@@ -18,8 +19,11 @@ $containerBuilder = new ContainerBuilder();
 $containerBuilder->useAutowiring(false);
 $containerBuilder->useAnnotations(false);
 $containerBuilder->addDefinitions([
-    HelloWorld::class => create(HelloWorld::class)->constructor(get('Foo')),
-    'Foo' => 'bar'
+    HelloWorld::class => create(HelloWorld::class)->constructor(get('Foo'), get('Response')),
+    'Foo' => 'bar',
+    'Response' => function() {
+        return new Response();
+    },
 ]);
 
 
